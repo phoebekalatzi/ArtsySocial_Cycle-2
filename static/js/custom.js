46cc5532e197f1d3235e565595efc85a0bc7f59b
40127093 <40127093@live.napier.ac.uk>
2017-10-29 10:58:46 -0700

    externalising all inline styles and scripts and resolving all issues with Content-Security-Policy header

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,4 @@
-
+var flag=1;
 function f1()
 {
     alert("Then you're on your own...Good luck with that!");
@@ -7,28 +7,26 @@ function f()
 {
   if(flag==1)
   {
-      Bn.style.top=90
-      Bn.style.left=500
-      flag=2
+      Bn.style.top=90;
+      Bn.style.left=500;
+      flag=2;
   }
   else if(flag==2)
   {
-      Bn.style.top=90
-      Bn.style.left=50
-      flag=3
+      Bn.style.top=90;
+      Bn.style.left=50;
+      flag=3;
    }
   else if(flag==3)
   {
-      Bn.style.top=235
-      Bn.style.left=360
-      flag=1
-      }
+      Bn.style.top=235;
+      Bn.style.left=360;
+      flag=1;
   }
+ }
 
-  document.addEventListener('DOMContentReady', function(){
-    document.getElementById('By')
-        .addEventListener('click', f1);
-        flag=1;
-    document.getElementById('Bn')
-        .addEventListener('mouseover', f);
-  });
+ window.addEventListener('DOMContentLoaded', function(){
+   document.getElementById('Iy').addEventListener('click', f1);
+   document.getElementById('In').addEventListener('mouseover',f);
+ });
+
