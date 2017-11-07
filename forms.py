db857b09967bdd745af3fbf25b7260793b3dcf7f
40127093 <40127093@live.napier.ac.uk>
2017-11-07 14:58:58 +0000

    enforcing longer and more complex passwords / moving the EqualTo built in validation to the password confirmation field

--- forms.py
+++ forms.py
@@ -19,6 +19,17 @@ def email_exists(form, field):
   if User.select().where(User.email == field.data).exists():
     raise ValidationError('User with that email already exists.')
 
+# custom validator to enforce secure passwords
+def password_check(form, field):
+
+   if(len(field.data)< 10 or len(field.data) > 128):
+     raise ValidationError('Password must be longer than 10 characters.')
+
+   if ((not any(x.isupper() for x in field.data)) or (not any(x.islower() for x in field.data))
+    or (not any(x.isdigit() for x in field.data))):
+       raise ValidationError('Your password needs at least 1 uppercase letter, 1 lowercase letter and 1 digit')
+
+
 class RegisterForm(FlaskForm):
    username = StringField(
      'Username...',
@@ -42,12 +53,13 @@ class RegisterForm(FlaskForm):
     'Password...',
      validators=[
        DataRequired(),
-       Length(min=2),
-       EqualTo('password2', message='Passwords must match')
+       password_check
     ])
+
    password2 = PasswordField(
       'Confirm Password...',
-       validators=[DataRequired()]
+      validators=[DataRequired(),
+      EqualTo('password', message='Passwords must match')]
  )   
 
 class LoginForm(FlaskForm):
